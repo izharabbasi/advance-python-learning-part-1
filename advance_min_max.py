bf7aa3026eedc893eb04f4090c248bb92103146f
Izhar Abaasi <izharabbasi103@gmail.com>
2020-05-07 12:14:29 +0500

    add second exmple

--- advance_min_max.py
+++ advance_min_max.py
@@ -1,3 +1,13 @@
 names = ["izhar", "sheryar", "ahmed"]
 
-print(max(names, key=lambda item: len(item)))
+# print(max(names, key=lambda item: len(item)))
+
+
+players = [
+    {"name": "izhar", "score": 90, "age": 20},
+    {"name": "sheryar", "score": 80, "age": 21},
+    {"name": "ahmed", "score": 60, "age": 25}
+]
+
+
+print(max(players, key=lambda item: item.get("age"))['name'])
