d0c93b43e29fd67cd47894bb9e7ce6148ea622c1
Izhar Abaasi <izharabbasi103@gmail.com>
2020-05-07 09:24:17 +0500

    complete task

--- iterable_vs_iterators.py
+++ iterable_vs_iterators.py
@@ -1,9 +1,15 @@
 numbers = [1, 2, 3, 4, 5, 6]
 
 sqaure = map(lambda a: a**2, numbers)
-
+# this is called iterator
 print(sqaure)
 print(next(sqaure))
 print(next(sqaure))
 print(next(sqaure))
 print(next(sqaure))
+
+# this is called iterables
+
+for i in numbers:
+    if i % 2 == 0:
+        print(i)
