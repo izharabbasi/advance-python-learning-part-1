f04814be89f96506ecd0dfbf3b67524d09022b69
Izhar Abaasi <izharabbasi103@gmail.com>
2020-05-07 09:21:10 +0500

    add iterator example

--- iterable_vs_iterators.py
+++ iterable_vs_iterators.py
@@ -1,1 +1,9 @@
-print("hello world")
+numbers = [1, 2, 3, 4, 5, 6]
+
+sqaure = map(lambda a: a**2, numbers)
+
+print(sqaure)
+print(next(sqaure))
+print(next(sqaure))
+print(next(sqaure))
+print(next(sqaure))
