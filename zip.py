ad7d831634034dd357d54e4970a462f824794124
Izhar Abaasi <izharabbasi103@gmail.com>
2020-05-07 10:38:12 +0500

    add description about zip

--- zip.py
+++ zip.py
@@ -1,5 +1,22 @@
 user_id = ["user1", "user2", "user3"]
 names = ["izhar", "sheryar", "ahmed"]
 
-print(list(zip(user_id, names)))
-print(dict(zip(user_id, names)))
+# print(list(zip(user_id, names)))
+# print(dict(zip(user_id, names)))
+
+l1 = [1, 2, 3, 4]
+l2 = [3, 4, 6, 7]
+l = [(1, 2), (3, 4), (5, 6), (7, 8)]
+newlist = []
+
+
+list(zip(*l))
+
+# print(l1)
+# print(l2)
+
+for pair in zip(l1, l2):
+    newlist.append(min(pair))
+
+
+print(newlist)
