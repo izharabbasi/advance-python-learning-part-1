85bb4a2dafbc9c9f4c2fd4293e948b85daf1287b
Izhar Abaasi <izharabbasi103@gmail.com>
2020-05-09 09:05:25 +0500

    add final statemnet

--- func_as_arguments.py
+++ func_as_arguments.py
@@ -4,4 +4,14 @@ def square(a):
 
 l = [1, 2, 3, 4]
 
-print(list(map(square, l)))
+# print(list(map(square, l)))
+
+
+def my_map(func, l):
+    new_list = []
+    for num in l:
+        new_list.append(func(num))
+    return new_list
+
+
+print(my_map(square, l))
